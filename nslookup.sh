105505f22559327b0c3e4f86129b24ac093dae98
cloud_devops <hnikhi@gmail.com>
2017-03-21 14:10:39 -0700

    Created nslookup.sh

---
+++ nslookup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+for IP in `cat /Users/nharinath/test/ips.txt`
+do
+        printf "$IP\t"
+
+        LOOKUP_RES=`nslookup $IP`
+
+        FAIL_COUNT=`echo $LOOKUP_RES | grep "** server can't find " | wc -l`;
+
+        if [ $FAIL_COUNT -eq 1 ]
+        then
+             NAME='Bad FQDN\n'; echo $NAME
+        else
+
+             nslookup $IP | awk -F"= " '/name/{print $2}' && printf '\n';
+        fi
+
+      #  echo $NAME
+
+done
